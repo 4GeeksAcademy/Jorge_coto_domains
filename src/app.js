eda8f73916227f066b698aad93374ec098d80713
JorgeCoto98 <120097112+JorgeCoto98@users.noreply.github.com>
2023-07-22 00:35:16 +0000

    Entrega

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "raccoon"];
+
+  function generateAllCombinations() {
+    var combinations = [];
+    for (var i = 0; i < pronoun.length; i++) {
+      for (var j = 0; j < adj.length; j++) {
+        for (var k = 0; k < noun.length; k++) {
+          var combination = pronoun[i] + adj[j] + noun[k] + ".com";
+          combinations.push(combination);
+        }
+      }
+    }
+    return combinations;
+  }
+
+  var allCombinations = generateAllCombinations();
+  var combinedText = allCombinations.join(", ");
+  document.getElementById("titulo").innerText = combinedText;
 };
